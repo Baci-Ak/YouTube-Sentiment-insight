d63bc52810f1f9d91f004075877f31d185117d94
Bassey Akom <bassi.cim@gmail.com>
2025-11-16 23:45:56 +0000

    added the ML pipeline

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Bassey Akom',
+    author_email= 'bassi.cim@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
